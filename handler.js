d2bb21c9b4e0901141abc6a074f53473172254c8
kokhowchuang <chuangkokhow@gmail.com>
2022-10-11 12:23:07 +0800

    done mysql connection

--- handler.js
+++ handler.js
@@ -2,16 +2,33 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
+const mysql = require("serverless-mysql")();
+
+mysql.config({
+  host: process.env.ENDPOINT,
+  database: process.env.DATABASE,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+});
+
 app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
 });
 
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
+app.get("/hello", async (req, res, next) => {
+  let results = await mysql.query("SELECT * FROM arrivo.User");
+
+  // Run clean up function
+  await mysql.end();
+
+  // Return the results
+  return res.status(200).json(results);
+
+  // return res.status(200).json({
+  //   message: "Hello from path!",
+  // });
 });
 
 app.use((req, res, next) => {
